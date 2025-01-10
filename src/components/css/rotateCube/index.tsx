9be366a3f7cea2dba97fe1c63bc8ebc1ce1cf043
杜新禄 <xinlu@ssc-hn.com>
2025-01-10 11:30:44 +0800

    feat: 实现旋转方块

--- src/components/css/rotateCube/index.tsx
+++ src/components/css/rotateCube/index.tsx
@@ -4,15 +4,19 @@ import styles from "./index.module.scss";
 export const RotateCube = () => {
   const component = (
     <div className={styles.spinner}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
     </div>
   );
-  const markdown = "";
+  const markdown = `
+- 正方体由6个div组成，表面颜色background-color，边颜色border-color
+- 设置每个div的初始角度组成立方体
+- animation开始旋转
+  `;
 
   return (
     <ComponentContainer
